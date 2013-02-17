f85969be276cc883b4bbb92423e98604beb9d3c1
Richard Tia <richie.tia@gmail.com>
2013-02-17 15:38:08 -0500

    Shuffle
    -Randomize name function
    -Shuffle function

---
+++ Shuffle/Shuffle/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuffle
+{
+    public static class ExtensionMethods
+    {
+        public static void Swap<T>(List<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+    }
+}
---
+++ Shuffle/Shuffle/Tools.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Shuffle
+{
+    class Tools
+    {
+
+        private static readonly Random RandomNumber = new Random();
+        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const int Size = 5;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shreds"></param>
+        /// <returns></returns>
+        public static List<Tuple<string,int>> Shuffle(List<string> shreds)
+        {
+            var pair;
+            List<Tuple<string,int>> orderedPair = new List<Tuple<string, int>>();
+            for (int ii=0; ii<shreds.Count; ii++ )
+            {
+                pair = Tuple.Create(shreds[ii], ii);
+                orderedPair.Add(pair);
+                var swapNumber = RandomNumber.Next(0, shreds.Count-1);
+                ExtensionMethods.Swap(orderedPair, ii, swapNumber);
+            }
+            return orderedPair;
+
+        }
+
+        /// <summary>
+        /// Creates a shred filename with 5 randoms characters appended to the end
+        /// </summary>
+        /// <returns></returns>
+        public static string RandomizeName()
+        {
+            char[] buffer = new char[Size];
+            for(int ii=0; ii<Size; ii++)
+            {
+                buffer[ii] = Alphabet[RandomNumber.Next(Alphabet.Length)];
+            }
+            string randomSuffix = new string(buffer);
+            string newFilename = "image" + randomSuffix + ".png";
+            return newFilename;
+        }
+
+
+        
+    }
+}
